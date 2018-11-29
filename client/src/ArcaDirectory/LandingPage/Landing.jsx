49782229fd3c1707d5ac0350c4b6f2d2256dc9a3
wadabender <alexshuseiwada@gmail.com>
2018-11-29 05:49:58 -0800

    fixed merge conflicts with David's changes, added carousel

--- client/src/ArcaDirectory/LandingPage/Landing.jsx
+++ client/src/ArcaDirectory/LandingPage/Landing.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../Navbar.jsx';
 import Main from './components/Main.jsx';
 import Footer from '../Footer.jsx';
+import { ReactiveBase, DataSearch} from '@appbaseio/reactivesearch'
 
 class Landing extends React.Component {
   constructor(props) {
@@ -14,7 +15,17 @@ class Landing extends React.Component {
   render() {
     return(
       <div>
+        <ReactiveBase
+          app="arca-housing-list"
+          credentials="gyt60WE3T:18fa7784-3c3b-45e5-a1c5-f54b2696506a"
+          type="listing"
+          theme={{
+            //Fix this to what the rest of the styling is later
+            primaryColor: '#A9A9A9'
+          }}
+        >
         <Navbar />
+        </ReactiveBase>
         <Main />
         <Footer />
       </div>
