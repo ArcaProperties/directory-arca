c5bc2680f5c5412b7c084792581abf244b7255f3
wadabender <alexshuseiwada@gmail.com>
2018-12-10 00:46:32 -0800

    changed port

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 
-let PORT = 7777;
+let PORT = process.env.PORT || 7777;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
